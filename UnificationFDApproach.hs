e904d467daef8b09983cf0086817bc737e495517
Arved <arved.friedemann@stoppe.de>
2020-07-29 16:56:17 +0200

    Made lots of pretty printing infrastructure and got a tiny kb to run. More investigation needed though into whether it does what I think it does

--- UnificationFDApproach.hs
+++ UnificationFDApproach.hs
@@ -15,6 +15,8 @@ import Data.Traversable
 import Data.List
 import Safe
 
+import Text.Parsec hiding (spaces)
+
 data Constant = TOP | BOT | CON String deriving (Show, Eq)
 data Term a = CONST Constant
             | APPL a a
@@ -42,10 +44,15 @@ con = UTerm . CONST
 appl :: OpenTerm -> OpenTerm -> OpenTerm
 appl a b = UTerm (APPL a b)
 
-lst :: [OpenTerm] -> OpenTerm
-lst [] = UTerm (CONST TOP)
-lst (x:y:[]) = UTerm (APPL x y)
-lst (x:xs) = UTerm (APPL x (lst xs))
+--TODO! This should be a foldl, not foldr! WARNING!
+list :: [OpenTerm] -> OpenTerm
+list [] = UTerm (CONST TOP)
+list (x:y:[]) = UTerm (APPL x y)
+list (x:xs) = UTerm (APPL x (list xs))
+
+clst :: [CTerm a] -> CTerm a
+clst [] = CCONST BOT
+clst ls = foldl1 CAPPL ls
 
 lookout :: (MonadState s m) => m a -> m a
 lookout m = do {
@@ -73,10 +80,101 @@ createOpenTerm t = do {
   return $ applyCBinding (zip vars intVars) t
 }
 
+fromOpenTerm :: OpenTerm -> CTerm IntVar
+fromOpenTerm (UVar i) = CVAR i
+fromOpenTerm (UTerm (CONST c)) = CCONST c
+fromOpenTerm (UTerm (APPL a b)) = CAPPL (fromOpenTerm a) (fromOpenTerm b)
+
+--not too sure if the OpenTerms are anything special...
 applyCBinding :: (Eq a) => [(a, OpenTerm)] -> CTerm a -> OpenTerm
 applyCBinding asm (CCONST c)  = con c
 applyCBinding asm (CVAR v)    = lookupJust v asm
 applyCBinding asm (CAPPL a b) = appl (applyCBinding asm a) (applyCBinding asm b)
 
+applyCBind :: (Eq a) => [(a, b)] -> CTerm a -> CTerm b
+applyCBind asm (CCONST c)  = CCONST c
+applyCBind asm (CVAR v)    = CVAR $ lookupJust v asm
+applyCBind asm (CAPPL a b) = CAPPL (applyCBind asm a) (applyCBind asm b)
+
+
 getPossibleMatches :: [OpenTerm] -> OpenTerm -> IntBindMon [OpenTerm]
 getPossibleMatches kb goal = concat <$> (sequence $ [catchE (lookout $ do {u <- unify t goal; return <$> ((applyBindings u) :: IntBindMon OpenTerm)}) (const $ return []) | t <- kb])
+
+
+
+---------------------------------------------------------
+--pretty printing
+---------------------------------------------------------
+niceNames = (take 26 (return <$> ['a'..])) ++ (map ("x" ++) (show <$> [1..]))
+
+giveNiceNames :: (Eq a) => CTerm a -> CTerm String
+giveNiceNames t = applyCBind asm t
+  where vars = nubVars t
+        asm = zip vars niceNames
+
+ppCTerm :: CTerm String -> String
+ppCTerm t = execWriter $ ppCTerm' t
+
+ppCTerm' :: CTerm String -> Writer String ()
+ppCTerm' (CCONST TOP) = tell "T"
+ppCTerm' (CCONST BOT) = tell "()"
+ppCTerm' (CCONST (CON s)) = tell s
+ppCTerm' (CVAR v) = tell v
+ppCTerm' (CAPPL a b@(CAPPL a' b')) = tell "(" >> ppCTerm' a >> tell ") " >> ppCTerm' b
+ppCTerm' (CAPPL a b) = ppCTerm' a >> tell " " >> ppCTerm' b
+
+bindConst :: [String] -> CTerm String -> CTerm String
+bindConst bnds (CCONST c) = CCONST c
+bindConst bnds (CVAR x)
+    | x `elem` bnds = CCONST (CON x)
+    | otherwise = CVAR x
+bindConst bnds (CAPPL a b) = CAPPL (bindConst bnds a) (bindConst bnds b)
+
+----------------------------------
+--testing stuff
+----------------------------------
+binds = bindConst ["=","^"]
+testkb = binds <$> rt <$> ["a = a", "b = b", "(a ^ (a -> b)) -> b"]
+testgoal = binds $ rt $ "(k k) = (l m)"
+
+test1 = runIntBind $ do {
+  kb <- sequence $ lift <$> createOpenTerm <$> testkb;
+  goal <- lift $ createOpenTerm testgoal;
+  mts <- getPossibleMatches kb goal;
+  return $ ppCTerm <$> giveNiceNames <$> fromOpenTerm <$> mts
+}
+
+----------------------------------
+--parsing
+----------------------------------
+
+forbiddenSymb = "() \t\n`"
+spaceChars = " \t"
+
+idfr::Parsec String st String
+idfr = many1 (noneOf forbiddenSymb)
+
+baseTerm::Parsec String st (CTerm String)
+baseTerm = (try $ string "()" >> return (CCONST BOT)) <|> (try $ parens term) <|> (CVAR <$> idfr)
+
+term::Parsec String st (CTerm String)
+term = clst <$> (spaces >> sepEndBy1 baseTerm spaces1)
+
+parens::Parsec String st a -> Parsec String st a
+parens p = do {string "("; r <- p; string ")"; return r}
+
+spaces::Parsec String st String
+spaces = many (oneOf spaceChars)
+
+spaces1::Parsec String st String
+spaces1 = many1 (oneOf spaceChars)
+
+safeParse::Parsec [a] () b -> [a] -> b
+safeParse p ipt = case parse p "" ipt of
+                    Right v -> v
+                    Left err -> error (show err)
+
+termFromString::String -> CTerm String
+termFromString = safeParse term
+rt::String -> CTerm String
+rt = termFromString
