64db0ffe888d3575687e69076e874ec38a5dd23a
Arved <arved.friedemann@stoppe.de>
2020-09-29 16:11:29 +0200

    Made some more notes. Preparing to have consecutive search state deduced by reflection

--- QuantifierApproach/Solver.hs
+++ QuantifierApproach/Solver.hs
@@ -62,10 +62,13 @@ testkb = [
   --TODO: Better: define when a predicate is deterministic on a certain KB, and that the aim is to find the right KB for the job
   --TODO: Determinism in execution only possible if no two rules overlap
   --TODO: The problem needs to be expressed on the actual state object...with unassigned variables and stuff...
-  "solve goal on kb in state with proof" --the state expresses which variables are assigned. This could also be done by assigning all unassigned variables with some constant 'unassigned'. 
+  "solve goal on kb in state with proof" --the state expresses which variables are assigned. This could also be done by assigning all unassigned variables with some constant 'unassigned'.
 
 ]
 testgoal = ["a v b"]
 
 
 --TODO: remove possibilities if they (immediately) imply falsity
+
+
+--IDEA: The next KB and goals need to be deduced as well. so every time there is a decision, the state still having decisions is quoted and deducing the next of it is placed as a goal too, and then further evaluated. the next state stays incomplete, but can be further propagated if possible. It might be that this changes the order of deductions, but that should be fine as the result is always the same modulo isomorphism. fact is, that each state is quoted, put as a goal and deduced further, and the next state is its unquoted version (where possible). This can be modeled by letting the unquote afloat if impossible, just putting a variable where there ought to be a value. Therefore, unquoting can just be a special predicate, as it would only work on a fully instantiated value anyway. 
